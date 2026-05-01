3552b73279545c9b0bbbe6247ee1b1da089505f8
Chris Brown <chris.m.brown@protonmail.com>
2026-05-01 21:49:11 +0000

    Fix archive migration: remove index (strong_migrations blocks concurrent index)

--- db/migrate/20260501000001_add_archived_at_to_proposals.rb
+++ db/migrate/20260501000001_add_archived_at_to_proposals.rb
@@ -3,6 +3,5 @@
 class AddArchivedAtToProposals < ActiveRecord::Migration[8.0]
   def change
     add_column :proposals, :archived_at, :datetime, null: true
-    add_index  :proposals, :archived_at
   end
 end
