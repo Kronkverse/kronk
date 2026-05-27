a29eda05bd4b295267683d9207787e2223b3da09
tal <talitamoss@proton.me>
2026-05-27 06:02:12 +0000

    api: expose proposal body in ProposalSummarySerializer

--- app/serializers/rest/proposal_summary_serializer.rb
+++ app/serializers/rest/proposal_summary_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class REST::ProposalSummarySerializer < ActiveModel::Serializer
-  attributes :id, :title, :summary, :status, :proposal_type,
+  attributes :id, :title, :body, :summary, :status, :proposal_type,
              :support_count, :veto_count, :participation_count,
              :categories, :created_at
 
